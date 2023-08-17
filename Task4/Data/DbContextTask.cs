c00801590d3379c3cf5b8d8b7c7134c7d8d424f6
idris <idrisulu98@gmail.com>
2023-08-17 11:34:16 +0300

    Add files via upload

---
+++ Task4/Data/DbContextTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Task4.Data
+{
+    public class DbContextTask : DbContext
+    {
+        //public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=qwe789asd;Server=localhost;Port=5432;Database=QuizDb;Integrated Security=true;Pooling=true;");
+        }
+
+        public DbSet<Quiz> Quiz { get; set; }
+        public DbSet<UserAnswer> UserAnswer { get; set; }
+
+    }
+}
